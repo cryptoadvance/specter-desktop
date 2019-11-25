eac166cee26250107cd80e7046ded9570a69af72
Kim Neunert <kneunert@gmail.com>
2019-11-25 18:02:30 +0100

    fixes #36 and some more testing

--- tests/test_specter.py
+++ tests/test_specter.py
@@ -1,14 +1,34 @@
-''' Tests for lovac.models '''
 import shutil
 import pytest
+import json
 from specter import Specter, alias
 
+
+
 @pytest.fixture
-def specter_not_configured():
+def empty_data_folder():
     # Make sure that this folder never ever gets a reasonable non-testing use-case
     data_folder = './test_specter_data_2789334'
     shutil.rmtree(data_folder, ignore_errors=True) 
-    yield Specter(data_folder=data_folder)    
+    yield data_folder
+    shutil.rmtree(data_folder, ignore_errors=True)
+
+@pytest.fixture
+def specter_regtest_configured(bitcoin_regtest):
+    # Make sure that this folder never ever gets a reasonable non-testing use-case
+    data_folder = './test_specter_data_3456778'
+    shutil.rmtree(data_folder, ignore_errors=True)
+    config = {
+        "rpc": {
+            "autodetect": False,
+            "user": bitcoin_regtest["rpc_username"],
+            "password": bitcoin_regtest["rpc_password"],
+            "port": bitcoin_regtest["rpc_port"],
+            "host": "localhost",
+            "protocol": "http"
+        },
+    }
+    yield Specter(data_folder=data_folder, config=config)
     shutil.rmtree(data_folder, ignore_errors=True)
 
 
@@ -17,14 +37,14 @@ def test_alias():
     assert alias("wurst_1") == "wurst_1"
     assert alias("Wurst$ 1") == "wurst_1"
 
-def test_specter_permrights():
-    with pytest.raises(Exception):
-        Specter("/notexisting_directory")
+def test_specter(specter_regtest_configured):
+    specter_regtest_configured.check()
+    assert specter_regtest_configured.wallets is not None
+    assert specter_regtest_configured.devices is not None
+    json_return = json.loads(specter_regtest_configured.test_rpc()["out"] )
+    assert json_return['blocks'] == 101
+    assert json_return['chain'] == 'regtest'
 
-def test_specter(specter_not_configured):
-    specter_not_configured.check()
-    assert specter_not_configured.wallets is not None
-    assert specter_not_configured.devices is not None
-    some_json = specter_not_configured.test_rpc()
-    assert some_json["out"] == ""
-    assert some_json["err"] == "autodetect failed"
+def test_device(empty_data_folder):
+    from specter import DeviceManager
+    my_dm = DeviceManager(data_folder=empty_data_folder)
