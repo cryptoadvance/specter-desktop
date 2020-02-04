27b79ad0aecaffb32675385e2c59aace32c4ab73
Kim Neunert <kneunert@gmail.com>
2020-02-04 07:06:13 +0100

    automate pip-packages-gen + upload for gittags release documentation proper package including and  static + templates

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup, find_packages
 from glob import glob
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name="specter-desktop", 
-    packages=find_packages('src/specter'),
-    package_dir={'': 'src/specter'}
+    name="specter-desktop",
+    version="v0.0.11",
+    author="Stepan Snigirev",
+    author_email="snigirev.stepan@gmail.com",
+    description="A GUI for Bitcoin Core optimised to work with airgapped hardware wallets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cryptoadvance/specter-desktop",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    # take METADATA.in into account, include that stuff as well (static/templates)
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Flask",
+    ],
+    python_requires='>=3.6',
 )
