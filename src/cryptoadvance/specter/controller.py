235f09934fcc42e10a43bf19ae46ef4d36146c7f
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-02-25 22:18:19 +0100

    Add rescan blockchain feature (#73)
    * rescanblockchain, a few style fixes, tor import fix
    
    * add keypool refill option to wallet settings
    
    * abort button width
    
    Co-authored-by: Kim Neunert <kneunert@gmail.com>

--- src/cryptoadvance/specter/controller.py
+++ src/cryptoadvance/specter/controller.py
@@ -341,13 +341,38 @@ def wallet_send(wallet_alias):
                                                 wallet_alias=wallet_alias, wallet=wallet, 
                                                 specter=app.specter, rand=rand, error=err)
 
-@app.route('/wallets/<wallet_alias>/settings/')
+@app.route('/wallets/<wallet_alias>/settings/', methods=['GET','POST'])
 def wallet_settings(wallet_alias):
     app.specter.check()
+    error = None
     try:
         wallet = app.specter.wallets.get_by_alias(wallet_alias)
     except:
         return render_template("base.html", error="Wallet not found", specter=app.specter, rand=rand)
+    if request.method == "POST":
+        action = request.form['action']
+        if action == "rescanblockchain":
+            startblock = int(request.form['startblock'])
+            try:
+                res = wallet.cli.rescanblockchain(startblock, timeout=1)
+                print(res)
+            except requests.exceptions.ReadTimeout:
+                pass
+            except Exception as e:
+                print(e)
+                error = "%r" % e
+            wallet.getdata()
+        elif action == "abortrescan":
+            res = wallet.cli.abortrescan()
+            if not res:
+                error="Failed to abort rescan. Maybe already complete?"
+            wallet.getdata()
+        elif action == "keypoolrefill":
+            delta = int(request.form['keypooladd'])
+            wallet.keypoolrefill(wallet["keypool"], wallet["keypool"]+delta)
+            wallet.keypoolrefill(wallet["change_keypool"], wallet["change_keypool"]+delta, change=True)
+            wallet.getdata()
+
     cc_file = None
     qr_text = wallet["name"]+"&"+descr(wallet)
     if wallet.is_multisig:
@@ -372,11 +397,13 @@ Format: {}
                             cc_file=urllib.parse.quote(cc_file), 
                             wallet_alias=wallet_alias, wallet=wallet, 
                             specter=app.specter, rand=rand, 
+                            error=error,
                             qr_text=qr_text)
     else:
         return render_template("wallet_settings.html", 
                             wallet_alias=wallet_alias, wallet=wallet, 
                             specter=app.specter, rand=rand, 
+                            error=error,
                             qr_text=qr_text)
 
 ################# devices management #####################
--- src/cryptoadvance/specter/logic.py
+++ src/cryptoadvance/specter/logic.py
@@ -11,7 +11,11 @@ from .helpers import deep_update, load_jsons
 from .rpc import RPC_PORTS, BitcoinCLI, autodetect_cli
 from .serializations import PSBT
 
-WALLET_CHUNK = 5
+# a gap of 20 addresses is what many wallets do
+WALLET_CHUNK = 20
+# we don't need to scan earlier than that 
+# as we don't support legacy wallets
+FIRST_SEGWIT_BLOCK = 481824
 
 purposes = OrderedDict({
     None: "General",
@@ -153,7 +157,7 @@ class Specter:
             return {"out": "", "err": "autodetect failed", "code": -1}
         r = {}
         try:
-            r["out"] = json.dumps(cli.getmininginfo(),indent=4)
+            r["out"] = json.dumps(cli.getblockchaininfo(),indent=4)
             r["err"] = ""
             r["code"] = 0
         except:
@@ -553,11 +557,25 @@ class Wallet(dict):
             self.transactions = self.cli.listtransactions("*", 20, 0, True)[::-1]
         except:
             self.transactions = None
+        try:
+            self.info = self.cli.getwalletinfo()
+        except:
+            self.info = None
         self._check_change()
         return {
             "balance": self.balance,
             "transactions": self.transactions
         }
+
+    @property
+    def rescan_progress(self):
+        """Returns None if rescanblockchain is not launched,
+           value between 0 and 1 otherwise
+        """
+        if self.info is None or "scanning" not in self.info or self.info["scanning"] == False:
+            return None
+        else:
+            return self.info["scanning"]["progress"]
 
     def getnewaddress(self):
         self._dict["address_index"] += 1
@@ -677,11 +695,10 @@ class Wallet(dict):
                 "range": [start, end], 
                 "timestamp": "now", 
                 "keypool": True, 
-                "watchonly": True,
-                "rescan": False
+                "watchonly": True
             }
         ]
-        r = self.cli.importmulti(args, timeout=120)
+        r = self.cli.importmulti(args, {"rescan": False}, timeout=120)
         # bip67 requires sorted public keys for multisig addresses
         if self.is_multisig:
             # we do one at a time
@@ -689,7 +706,7 @@ class Wallet(dict):
             for i in range(start, end):
                 sorted_desc = self.sort_descriptor(self[desc], i)
                 args[0]["desc"] = sorted_desc
-                self.cli.importmulti(args, timeout=120)
+                self.cli.importmulti(args, {"rescan": False}, timeout=120)
         self._dict[pool] = end
         self._commit(update_manager=False)
         return end
