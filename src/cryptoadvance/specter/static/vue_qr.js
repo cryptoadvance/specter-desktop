27f93437436aa16dc8d378cf2e8513984f8edb29
gorazdko <gorazdko@gmail.com>
2020-04-19 22:04:55 +0200

    qrcode: toggle between static and dynamic qr mode on a mouse click

--- src/cryptoadvance/specter/static/vue_qr.js
+++ src/cryptoadvance/specter/static/vue_qr.js
@@ -1,11 +1,14 @@
 Vue.component('qrencode',{
-  template : '<div><qrcode v-bind:value="qrval" v-bind:options="wd"></qrcode></div>',
+  template : '<div :title="title" v-on:click="clicked()"><qrcode :value="qrval" :options="wd"></qrcode></div>',
   data: function() {
      return {
         speed : 300,
         indx : 0,
         qrval : "",
         chunks : [],
+        title : "",
+        isQRlarge : false,
+        isQRplaying : false,
         wd : { width: 400 }
      }
   },
@@ -14,6 +17,7 @@ Vue.component('qrencode',{
     this.split()
     this.qrval=this.text
     this.wd = {width: this.width}
+    if (this.isQRlarge) {this.title = "Click to animate!"}
   },
   mounted() {this.init()},
   methods:{
@@ -21,6 +25,7 @@ Vue.component('qrencode',{
       var max_len = 300
       txt_len = this.text.length
       if (txt_len / max_len > 1.0) {
+        this.isQRlarge = true
         let number_of_chunks = Math.ceil(txt_len / max_len)
         n = Math.ceil(txt_len / number_of_chunks)
         for (let i = 0; i < txt_len; i += n) {
@@ -33,17 +38,28 @@ Vue.component('qrencode',{
       else {
         this.chunks.push(this.text)
       }
-     },
+    },
     animate : function() {
-      this.indx++
-      if (this.indx >= this.chunks.length) {
-        this.indx = 0
+      if (this.isQRplaying == false) {
+        this.qrval = this.text
+        this.title = "Click to animate!"
       }
-      this.qrval = this.chunks[this.indx]
+      else {
+        this.indx++
+        if (this.indx >= this.chunks.length) {this.indx = 0}
+        this.qrval = this.chunks[this.indx]
+        this.title = "Click to stop animation!"
+      }
+      if (!this.isQRlarge) {
+        this.title = ""
+      }
     },
     init : function() {
       const self=this
       setInterval(self.animate, this.speed)
+    },
+    clicked : function() {
+      this.isQRplaying = !this.isQRplaying
     }
   }
 });
