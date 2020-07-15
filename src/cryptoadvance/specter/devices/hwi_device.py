ca3234192f6f6be396c4ff497e161dd76f733d72
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-07-15 21:28:28 +0200

    Xpubs in PSBT (#232)
    add xpub fields to hwi, fix endian

--- src/cryptoadvance/specter/devices/hwi_device.py
+++ src/cryptoadvance/specter/devices/hwi_device.py
@@ -1,6 +1,5 @@
 from ..device import Device
 
-
 class HWIDevice(Device):
     def __init__(self, name, alias, device_type, keys, fullpath, manager):
         Device.__init__(self, name, alias, device_type, keys, fullpath, manager)
@@ -8,5 +7,4 @@ class HWIDevice(Device):
         self.exportable_to_wallet = False
 
     def create_psbts(self, base64_psbt, wallet):
-        # fills non_witness_utxo for all inputs
         return { 'hwi': wallet.fill_psbt(base64_psbt) }
