331c9bf1cd16dc7623e4bb625859dd03d20b78d6
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-08-23 11:49:07 +0200

    remove non-witness for elecrtum

--- src/cryptoadvance/specter/devices/electrum.py
+++ src/cryptoadvance/specter/devices/electrum.py
@@ -14,8 +14,10 @@ class Electrum(Device):
         super().__init__(name, alias, keys, fullpath, manager)
 
     def create_psbts(self, base64_psbt, wallet):
+        # remove non_witness utxo for QR code
+        updated_psbt = wallet.fill_psbt(base64_psbt, non_witness=False, xpubs=False)
         psbts = {
-            'qrcode': b43_encode(a2b_base64(base64_psbt)),
+            'qrcode': b43_encode(a2b_base64(updated_psbt)),
             'sdcard': base64_psbt,
         }
         return psbts
