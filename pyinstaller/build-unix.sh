8f2a655692d15f81b1426e1c7d9eeb2015022aa8
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-08-28 11:28:52 +0200

    Build scripts (#319)

---
+++ pyinstaller/build-unix.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# pass version number as an argument 
+
+pip install -e ..
+pip install -r requirements.txt
+rm -rf build/ dist/ specterd/ release/
+pyinstaller specterd_onedir.spec
+mv dist/specterd/ ./specterd
+pyinstaller specter_desktop.spec
+pyinstaller specterd.spec
+
+mkdir release
+
+mkdir release/specter_desktop-$1-`arch`-linux-gnu
+cp dist/Specter release/specter_desktop-$1-`arch`-linux-gnu/
+cp -r ../udev release/specter_desktop-$1-`arch`-linux-gnu/udev
+tar -czvf release/specter_desktop-$1-`arch`-linux-gnu.tar.gz release/specter_desktop-$1-`arch`-linux-gnu
+
+mkdir release/specterd-$1-`arch`-linux-gnu
+cp dist/specterd release/specterd-$1-`arch`-linux-gnu/
+cp -r ../udev release/specterd-$1-`arch`-linux-gnu/udev
+tar -czvf release/specterd-$1-`arch`-linux-gnu.tar.gz release/specterd-$1-`arch`-linux-gnu
