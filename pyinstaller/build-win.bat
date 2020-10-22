dea7876b89e22dc6ab5cc0e0871fa003f75ab269
benk10 <ben.kaufman10@gmail.com>
2020-10-22 09:29:53 +0300

    Fix windows release script

--- pyinstaller/build-win.bat
+++ pyinstaller/build-win.bat
@@ -8,15 +8,23 @@ rmdir /s /q .\release\
 pyinstaller.exe specterd.spec
 cd electron
 call npm ci
-if (%2%=='make-hash') (
-    call node ./set-version %1% ../specterd.exe
+if "%2%"=="make-hash" (
+    call node ./set-version "%1%" "../dist/specterd.exe"
 ) else (
-    node ./set-version %1%
+    node ./set-version "%1%"
 )
 call npm i
 call npm run dist
+cd ..
 
 
 mkdir release
+SET EXE_PATH="electron\dist\Specter Setup %1%.exe"
+SET EXE_RELEASE_PATH="release\Specter Setup %1%.exe"
+echo f | xcopy /s/y %EXE_PATH%  %EXE_RELEASE_PATH%
+
+SET SPECTERD_PATH="dist\specterd.exe"
+SET SPECTERD_RELEASE_PATH="release\specterd.exe"
+echo f | xcopy /s/y %SPECTERD_PATH%  %SPECTERD_RELEASE_PATH%
 
 echo We've built everything we could, now zip specterd and run inno-setup for specter-desktop
