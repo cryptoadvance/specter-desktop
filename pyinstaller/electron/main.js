455f12b620fd90f4a89be2a8e530a4ed00f60e76
benk10 <ben.kaufman10@gmail.com>
2020-11-01 12:43:08 +0200

    Fix download + download version check settings

--- pyinstaller/electron/main.js
+++ pyinstaller/electron/main.js
@@ -20,7 +20,7 @@ const download = (uri, filename, callback) => {
     request.head(uri, (err, res, body) => {
         console.log('content-type:', res.headers['content-type'])
         console.log('content-length:', res.headers['content-length'])
-        if (res.statusCode == 200) {
+        if (res.statusCode != 404) {
           request(uri).pipe(fs.createWriteStream(filename)).on('close', callback)
         } else {
           callback(true)
