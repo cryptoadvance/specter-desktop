fdcd6698e529bd1909de34f4847470b6efaee8d5
Kim Neunert <k9ert@gmx.de>
2020-11-10 17:21:33 +0100

    release-script

---
+++ utils/release.sh
@@ -0,0 +1,100 @@
+#!//bin/bash
+
+ask_yn() {
+   while true; do
+        read -p "Is this correct [y/n]" yn
+        case $yn in
+            [Yy]* ) return 0 ;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done 
+}
+
+
+[ "$(git remote -v | grep upstream | grep 'git@github.com:cryptoadvance/specter-desktop.git' | wc -l)" = "2" ] || exit 2
+
+echo "    --> Fetching all tags ..."
+git fetch upstream --tags
+
+latest_version=$(git tag -l "v*" | grep -v 'pre' | tail -1)
+
+echo "latest_version is $latest_version. "
+
+echo "    --> The latest version is $latest_version. "
+echo "    --> We'll create the release-notes based on that."
+if ! ask_yn ; then
+    echo "break"
+    exit 2
+fi
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$current_branch" != "master" ]; then
+    echo "You're currently not on the master-branch, so we assume this will get a PR"
+    git fetch upstream master
+    PR_MODE=yes
+    if ! git diff master..${current_branch} --exit-code ; then
+        echo "You current branch is also not similiar to master."
+        echo "should i merge master?"
+        if ! ask_yn ; then
+            echo "break"
+            exit 2
+        fi
+    fi
+fi
+
+if [ -z $GH_TOKEN ]; then
+    echo "Your github-token is missing. Please export them like:"
+    echo "export GH_TOKEN="
+    exit 2
+fi
+
+echo "What should be the new version? Type in please:"
+read new_version
+if ! [[ $new_version =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+))?$ ]]; then 
+    echo "Does not match the pattern!"
+    exit 1; 
+fi
+
+
+echo "Here are the release-notes:"
+echo "--------------------------------------------------"
+echo "## $new_version} $(date +'%B %d, %Y')" > docs/new_release_notes.md
+docker run registry.gitlab.com/cryptoadvance/specter-desktop/github-changelog:latest --github-token $GH_TOKEN --branch master cryptoadvance specter-desktop $latest_version >> docs/new_release_notes.md
+echo "" >> docs/new_release_notes.md
+cat docs/new_release_notes.md
+echo "--------------------------------------------------"
+
+cp docs/release-notes.md docs/release-notes.md.orig
+cat docs/new_release_notes.md docs/release-notes.md.orig >  docs/release-notes.md
+rm docs/release-notes.md.orig docs/new_release_notes.md
+
+echo "Please check your new File and modify as you find approriate!"
+echo "We're waiting here ..."
+
+echo "Should we commit and push that now? "
+
+if ! ask_yn ; then
+    echo "break"
+    echo "Rolling back release_notes.md ..."
+    git checkout docs/release-notes.md
+    exit 2
+fi
+
+git add docs/release-notes.md
+git commit -m "adding release_notes to $new_version"
+git push
+
+if [ -n $PR_MODE ]; then
+    echo "Now go ahead and make your PR:"
+    echo "https://github.com/cryptoadvance/specter-desktop/compare"
+else
+    echo "Should i now create the tag $new_version ?"
+    if ! ask_yn ; then
+        echo "break"
+        exit 2
+    fi
+    git tag $new_version && git push upstream $new_version
+fi
+
