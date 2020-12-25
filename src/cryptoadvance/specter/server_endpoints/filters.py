cefa75f758b0117601c81c8bf2b0d17bba38edf2
figgyfigs <63835319+figgyfigs@users.noreply.github.com>
2020-12-25 10:50:14 -0800

    Update src/cryptoadvance/specter/server_endpoints/filters.py
    Co-authored-by: benk10 <ben.kaufman10@gmail.com>

--- src/cryptoadvance/specter/server_endpoints/filters.py
+++ src/cryptoadvance/specter/server_endpoints/filters.py
@@ -59,17 +59,15 @@ def btcunitamount(context, value):
 @filters_bp.app_template_filter("altunit")
 def altunit(context, value):
     if app.specter.price_check and (app.specter.alt_rate and app.specter.alt_symbol):
-        if app.specter.alt_symbol == "€":
-            return (
-                "{:,.2f}".format(float(value) * float(app.specter.alt_rate))
-                .rstrip("0")
-                .rstrip(".")
-                + app.specter.alt_symbol
-            )
+       rate = (
+            "{:,.2f}".format(float(value) * float(app.specter.alt_rate))
+            .rstrip("0")
+            .rstrip(".")
+        )
+        if app.specter.alt_symbol in ["$", "£"]:
+            return app.specter.alt_symbol + rate
         else:
-            return app.specter.alt_symbol + "{:,.2f}".format(
-                float(value) * float(app.specter.alt_rate)
-            ).rstrip("0").rstrip(".")
+            return rate + app.specter.alt_symbol
     return ""
 
 
