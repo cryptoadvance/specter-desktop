cabd8369f6ed12402e286075694376718a0b118e
kdmukai <keith.mukai@essaytagger.com>
2021-03-06 10:15:46 -0600

    Re-applying config change to pass Black formatting

--- src/cryptoadvance/specter/config.py
+++ src/cryptoadvance/specter/config.py
@@ -71,6 +71,9 @@ class BaseConfig(object):
 class DevelopmentConfig(BaseConfig):
     # https://stackoverflow.com/questions/22463939/demystify-flask-app-secret-key
     SECRET_KEY = "development key"
+    SPECTER_DATA_FOLDER = os.path.expanduser(
+        os.getenv("SPECTER_DATA_FOLDER", "~/.specter_dev")
+    )
 
 
 class TestConfig(BaseConfig):
