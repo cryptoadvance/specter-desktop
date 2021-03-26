7893459ffc87d05a6fbc7f66dac5bf71c3e64d6b
benk10 <ben.kaufman10@gmail.com>
2021-03-26 15:30:07 +0300

    Bugfix: Update Bitcoin Core data dir even when installed and update Cobo Vault instructions (#1056)
    * Update Cobo Vault instructions
    
    * Update Bitcoin Core data dir even when installed

--- src/cryptoadvance/specter/server_endpoints/controller.py
+++ src/cryptoadvance/specter/server_endpoints/controller.py
@@ -173,13 +173,14 @@ def setup_bitcoind():
     bitcoind_setup_status = app.specter.config.get(
         "bitcoind_setup", {"stage_progress": -1}
     )
+    app.specter.config["rpc"]["datadir"] = request.form.get(
+        "bitcoin_core_datadir", app.specter.config["rpc"]["datadir"]
+    )
+    app.specter._save()
     if (
         not os.path.isfile(app.specter.bitcoind_path)
         and bitcoind_setup_status["stage_progress"] == -1
     ):
-        app.specter.config["rpc"]["datadir"] = request.form.get(
-            "bitcoin_core_datadir", app.specter.config["rpc"]["datadir"]
-        )
         app.specter.config["bitcoind_setup"]["stage_progress"] = 0
         app.specter.config["bitcoind_setup"]["error"] = ""
         app.specter._save()
