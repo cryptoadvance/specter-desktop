33d6c56146576f8b0760637deebc007ea7d25b51
kdmukai <keith.mukai@essaytagger.com>
2021-07-03 08:02:33 -0500

    Compiled in latest French translations

--- src/cryptoadvance/specter/server_endpoints/wallets.py
+++ src/cryptoadvance/specter/server_endpoints/wallets.py
@@ -124,7 +124,7 @@ def new_wallet_type():
         app.specter.rpc.listwallets()
     except Exception:
         err = _(
-            '<p><br>Configure Bitcoin Core is running with wallets disabled.<br><br>Please make sure disablewallet is off (set disablewallet=0 in your bitcoin.conf), then restart Bitcoin Core and try again.<br>See <a href="https://github.com/cryptoadvance/specter-desktop/blob/34ca139694ecafb2e7c2bd5ad5c4ac74c6d11501/docs/faq.md#im-not-sure-i-want-the-bitcoin-core-wallet-functionality-to-be-used-is-that-mandatory-if-so-is-it-considered-secure" target="_blank" style="color: white;">here</a> for more information.</p>'
+            '<p><br>Bitcoin Core is running with wallets disabled.<br><br>Please make sure disablewallet is off (set disablewallet=0 in your bitcoin.conf), then restart Bitcoin Core and try again.<br>See <a href="https://github.com/cryptoadvance/specter-desktop/blob/34ca139694ecafb2e7c2bd5ad5c4ac74c6d11501/docs/faq.md#im-not-sure-i-want-the-bitcoin-core-wallet-functionality-to-be-used-is-that-mandatory-if-so-is-it-considered-secure" target="_blank" style="color: white;">here</a> for more information.</p>'
         )
         return render_template("base.jinja", error=err, specter=app.specter, rand=rand)
     return render_template(
