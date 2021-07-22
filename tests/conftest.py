35226a483da633574e22c1e428ce8d8373484fc0
Kim Neunert <k9ert@gmx.de>
2021-07-22 16:35:46 +0200

    fix hanging test

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,13 +24,36 @@ from cryptoadvance.specter.server import create_app, init_app
 from cryptoadvance.specter.specter import Specter
 from cryptoadvance.specter.user import User
 from cryptoadvance.specter.util.wallet_importer import WalletImporter
+import code, traceback, signal
 
 pytest_plugins = ["ghost_machine"]
+
+# This is from https://stackoverflow.com/questions/132058/showing-the-stack-trace-from-a-running-python-application
+# it enables stopping a hanging test via sending the pytest-process a SIGUSR2 (12)
+# kill 12 pid-of-pytest
+# In the article they claim to open a debug-console which didn't work for me but at least
+# you get a stacktrace in the output.
+def debug(sig, frame):
+    """Interrupt running process, and provide a python prompt for
+    interactive debugging."""
+    d = {"_frame": frame}  # Allow access to frame object.
+    d.update(frame.f_globals)  # Unless shadowed by global
+    d.update(frame.f_locals)
+
+    i = code.InteractiveConsole(d)
+    message = "Signal received : entering python shell.\nTraceback:\n"
+    message += "".join(traceback.format_stack(frame))
+    i.interact(message)
+
+
+def listen():
+    signal.signal(signal.SIGUSR2, debug)  # Register handler
 
 
 def pytest_addoption(parser):
     """Internally called to add options to pytest
     see pytest_generate_tests(metafunc) on how to check that
+    Also used to register the SIGUSR2 (12) as decribed in conftest.py
     """
     parser.addoption("--docker", action="store_true", help="run bitcoind in docker")
     parser.addoption(
@@ -45,6 +68,7 @@ def pytest_addoption(parser):
         default="master",
         help="Version of elementsd (something which works with git checkout ...)",
     )
+    listen()
 
 
 def pytest_generate_tests(metafunc):
@@ -367,6 +391,7 @@ def specter_regtest_configured(bitcoin_regtest, devices_filled_data_folder):
     bitcoin_regtest.mine()
     # Realize that the wallet has funds:
     wallet.update()
+    assert wallet.fullbalance >= 20
     assert not specter.wallet_manager.working_folder is None
     try:
         yield specter
