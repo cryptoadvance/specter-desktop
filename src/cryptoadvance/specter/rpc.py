cryptoadvance/specter-desktop

commit 22cd3e9372abd0f95ce679d02711fb2bde86e126

Update rpc None error (#1333)

src/cryptoadvance/specter/rpc.py
@@ -40,6 +40,7 @@ def get_rpcconfig(datadir=get_default_datadir()):
         "cookies": [],
     }
     if not os.path.isdir(datadir):  # we don't know where to search for files
+        logger.warning(f"{datadir} not found")
         return config
     # load content from bitcoin.conf
     bitcoin_conf_file = os.path.join(datadir, "bitcoin.conf")
@@ -112,8 +113,6 @@ def get_configs(config=None, datadir=get_default_datadir()):
 
 
 def detect_rpc_confs(config=None, datadir=get_default_datadir()):
-    if config is None:
-        config = get_rpcconfig(datadir=datadir)
     rpcconfs = get_configs(config)
     rpc_arr = []
     for conf in rpcconfs:
@@ -185,7 +184,7 @@ def autodetect_rpc_confs(
 
 
 class RpcError(Exception):
-    """Specifically created for error-handling of the BitcoiCore-API
+    """Specifically created for error-handling of the BitcoinCore-API
     if thrown, check for errors like this:
     try:
         rpc.does_not_exist()

src/cryptoadvance/specter/node.py
@@ -145,7 +145,8 @@ class Node:
                 rpc = BitcoinRPC(
                     **rpc_conf_arr[0], proxy_url=self.proxy_url, only_tor=self.only_tor
                 )
-            # autodetect won't result in any logging, even if None
+            if rpc == None:
+                logger.warning(f"No rpc was found for {self}")
             return rpc
         else:
             # if autodetect is disabled and port is not defined
@@ -280,7 +281,7 @@ class Node:
                 logger.exception("Exception %s while check_info()" % e)
         else:
             if self.rpc is None:
-                logger.error(f"connection of {self} is None in check_info")
+                logger.warning(f"connection of {self} is None in check_info")
             elif not self.rpc.test_connection():
                 logger.debug(
                     f"connection {self.rpc} failed test_connection in check_info:"

commit 695515939367b2bf5d8b3ef645e266779e213e01

Use the configured bitcoind data directory
Fixes a regression introduced by https://github.com/cryptoadvance/specter-desktop/commit/22cd3e9372abd0f95ce679d02711fb2bde86e126#diff-04de6bab3ea93d6516d4769480f8ab06fa7e2eb339fc1c0dd302292fa820fd94L115

src/cryptoadvance/specter/rpc.py
@@ -113,7 +113,7 @@ def get_configs(config=None, datadir=get_default_datadir()):
 
 
 def detect_rpc_confs(config=None, datadir=get_default_datadir()):
-    rpcconfs = get_configs(config)
+    rpcconfs = get_configs(config, datadir)
     rpc_arr = []
     for conf in rpcconfs:
         rpc_arr.append(conf)