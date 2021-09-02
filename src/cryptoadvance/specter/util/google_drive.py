8b3a4e705360cb731b634f9c7ca2b81bb0de1163
Hani Mohammed <hani.m17@iiits.in>
2021-09-03 00:31:48 +0530

    Remove unused keys in CLIENT_CONFIG

--- src/cryptoadvance/specter/util/google_drive.py
+++ src/cryptoadvance/specter/util/google_drive.py
@@ -23,10 +23,7 @@ CLIENT_CONFIG = {
         "project_id": os.environ.get("GOOGLE_OAUTH_PROJECT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_secret": os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET"),
-        "redirect_uris": ["http://localhost:25441/"],
-        "javascript_origins": ["http://localhost:25441"],
     }
 }
 SCOPES = ["https://www.googleapis.com/auth/drive.appdata"]
