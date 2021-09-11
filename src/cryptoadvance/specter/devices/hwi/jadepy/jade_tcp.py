c27b171abe440a39e4def6d65ef18110e3925aa1
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-09-11 22:16:15 +0200

    enable jade for multisig

--- src/cryptoadvance/specter/devices/hwi/jadepy/jade_tcp.py
+++ src/cryptoadvance/specter/devices/hwi/jadepy/jade_tcp.py
@@ -2,7 +2,7 @@ import socket
 import logging
 
 
-logger = logging.getLogger('jade.tcp')
+logger = logging.getLogger("jade.tcp")
 
 
 #
@@ -19,7 +19,7 @@ logger = logging.getLogger('jade.tcp')
 # (caveat cranium)
 #
 class JadeTCPImpl:
-    PROTOCOL_PREFIX = 'tcp:'
+    PROTOCOL_PREFIX = "tcp:"
 
     @classmethod
     def isSupportedDevice(cls, device):
@@ -34,15 +34,15 @@ class JadeTCPImpl:
         assert self.isSupportedDevice(self.device)
         assert self.tcp_sock is None
 
-        logger.info('Connecting to {}'.format(self.device))
+        logger.info("Connecting to {}".format(self.device))
         self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        url = self.device[len(self.PROTOCOL_PREFIX):].split(':')
+        url = self.device[len(self.PROTOCOL_PREFIX) :].split(":")
         self.tcp_sock.connect((url[0], int(url[1])))
         assert self.tcp_sock is not None
 
         self.tcp_sock.__enter__()
-        logger.info('Connected')
+        logger.info("Connected")
 
     def disconnect(self):
         assert self.tcp_sock is not None
--- src/cryptoadvance/specter/devices/jade.py
+++ src/cryptoadvance/specter/devices/jade.py
@@ -1,6 +1,7 @@
 from . import DeviceTypes
 from .hwi_device import HWIDevice
-from .hwi.jade import JadeClient, enumerate
+from .hwi.jade import JadeClient
+from .hwi.jade import enumerate as jade_enumerate
 
 
 class Jade(HWIDevice):
@@ -17,14 +18,4 @@ class Jade(HWIDevice):
 
     @classmethod
     def enumerate(cls, *args, **kwargs):
-        return enumerate(*args, **kwargs)
-
-    def has_key_types(self, wallet_type, network="main"):
-        if wallet_type == "multisig":
-            return False
-        return super().has_key_types(wallet_type, network)
-
-    def no_key_found_reason(self, wallet_type, network="main"):
-        if wallet_type == "multisig":
-            return "Jade does not yet support multisig wallets."
-        return super().no_key_found_reason(wallet_type, network)
+        return jade_enumerate(*args, **kwargs)
