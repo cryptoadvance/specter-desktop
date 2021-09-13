b9f3916a464d87627e1e8213f0881346274b0e58
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-09-13 16:13:53 +0200

    disable jade multisig on Liquid

--- src/cryptoadvance/specter/devices/jade.py
+++ src/cryptoadvance/specter/devices/jade.py
@@ -2,6 +2,7 @@ from . import DeviceTypes
 from .hwi_device import HWIDevice
 from .hwi.jade import JadeClient
 from .hwi.jade import enumerate as jade_enumerate
+from ..helpers import is_liquid
 
 
 class Jade(HWIDevice):
@@ -20,3 +21,13 @@ class Jade(HWIDevice):
     @classmethod
     def enumerate(cls, *args, **kwargs):
         return jade_enumerate(*args, **kwargs)
+
+    def has_key_types(self, wallet_type, network="main"):
+        if wallet_type == "multisig" and is_liquid(network):
+            return False
+        return super().has_key_types(wallet_type, network)
+
+    def no_key_found_reason(self, wallet_type, network="main"):
+        if wallet_type == "multisig" and is_liquid(network):
+            return "Jade does not support multisig wallets on Liquid."
+        return super().no_key_found_reason(wallet_type, network)
