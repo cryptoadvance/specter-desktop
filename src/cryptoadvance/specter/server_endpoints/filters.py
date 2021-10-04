1185234cc8d4c33ad08f0dd73d18b5246faf4319
kdmukai <keith.mukai@essaytagger.com>
2021-10-04 17:03:29 -0500

    Changes `contextfilter` to `pass_context`
    Eliminates the deprecation warnings on `filters.py`:
    
    ```
    DeprecationWarning: 'contextfilter' is renamed to 'pass_context', the old name will be removed in Jinja 3.1.
    ```

--- src/cryptoadvance/specter/server_endpoints/filters.py
+++ src/cryptoadvance/specter/server_endpoints/filters.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import current_app as app
 from flask import Blueprint
-from jinja2 import contextfilter
+from jinja2 import pass_context
 from ..helpers import to_ascii20
 from ..liquid.util.pset import to_canonical_pset
 
@@ -10,19 +10,19 @@ filters_bp = Blueprint("filters", __name__)
 ############### filters ##################
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("ascii20")
 def ascii20(context, name):
     return to_ascii20(name)
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("datetime")
 def timedatetime(context, s):
     return format(datetime.fromtimestamp(s), "%d.%m.%Y %H:%M")
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("btcamount")
 def btcamount(context, value):
     if value < 0:
@@ -31,20 +31,20 @@ def btcamount(context, value):
     return "{:,.8f}".format(value).rstrip("0").rstrip(".")
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("to_canonical")
 def to_canonical(context, psbt):
     return to_canonical_pset(psbt)
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("btc2sat")
 def btc2sat(context, value):
     value = int(round(float(value) * 1e8))
     return f"{value}"
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("feerate")
 def feerate(context, value):
     value = float(value) * 1e8
@@ -55,7 +55,7 @@ def feerate(context, value):
     return "{:,.2f}".format(value).rstrip("0").rstrip(".")
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("btcunitamount")
 def btcunitamount(context, value):
     if app.specter.hide_sensitive_info:
@@ -68,7 +68,7 @@ def btcunitamount(context, value):
     return "{:,.0f}".format(round(value * 1e8))
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("altunit")
 def altunit(context, value):
     if app.specter.hide_sensitive_info:
@@ -88,14 +88,14 @@ def altunit(context, value):
     return ""
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("bytessize")
 def bytessize(context, value):
     value = float(value)
     return "{:,.0f}".format(value / float(1 << 30)) + " GB"
 
 
-@contextfilter
+@pass_context
 @filters_bp.app_template_filter("assetlabel")
 def assetlabel(context, asset):
     if app.specter.hide_sensitive_info:
