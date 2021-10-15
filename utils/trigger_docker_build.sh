2cd0ab95bd77e36a0a0e4b3bfa22bc6066308d1b
Kim Neunert <k9ert@gmx.de>
2021-10-16 01:32:05 +0200

    kick

--- utils/trigger_docker_build.sh
+++ utils/trigger_docker_build.sh
@@ -2,4 +2,7 @@
 
 payload="{\"ref\":\"master\", \"inputs\": {\"tag\": \"${CI_COMMIT_TAG}\"}}"
 curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${AARON_TOKEN}" \
-    https://api.github.com/repos/lncm/docker-specter-desktop/actions/workflows/dispatch.yml/dispatches  -d $payload
+    https://api.github.com/repos/lncm/docker-specter-desktop/actions/workflows/dispatch.yml/dispatches  -d @<(cat <<EOF
+$payload
+EOF
+)
