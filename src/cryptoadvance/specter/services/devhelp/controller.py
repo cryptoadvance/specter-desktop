202bb3c7cfd3f21328ffe2131373e5dab0810c1e
Kim Neunert <k9ert@gmx.de>
2022-02-06 21:23:03 +0100

    moar structure and tx-table refactoring

--- src/cryptoadvance/specter/services/devhelp/controller.py
+++ src/cryptoadvance/specter/services/devhelp/controller.py
@@ -1,10 +1,11 @@
 import logging
-from flask import Flask, Response, redirect, render_template, request, url_for, flash
-from flask_login import login_required
+from flask import Flask, Response, redirect, render_template, request, url_for
+from flask import current_app as app
+from flask_login import login_required, current_user
 
 from ..controller import user_secret_decrypted_required
 from .service import DevhelpService
-
+from ...wallet import Wallet
 
 """
     Empty placeholder just so the dummyservice/static folder can be wired up to retrieve its img
@@ -23,10 +24,49 @@ def index():
         "devhelp/index.jinja",
     )
 
+
 @devhelp_endpoint.route("/html/<html_component>")
 @login_required
 @user_secret_decrypted_required
 def html_component(html_component):
+    associated_wallet: Wallet = DevhelpService.get_associated_wallet()
     return render_template(
         f"devhelp/html/{html_component}",
+        wallet=associated_wallet,
+        services=app.specter.service_manager.services,
     )
+
+
+@devhelp_endpoint.route("/settings", methods=["GET"])
+@login_required
+@user_secret_decrypted_required
+def settings_get():
+    associated_wallet: Wallet = DevhelpService.get_associated_wallet()
+
+    # Get the user's Wallet objs, sorted by Wallet.name
+    wallet_names = sorted(current_user.wallet_manager.wallets.keys())
+    wallets = [current_user.wallet_manager.wallets[name] for name in wallet_names]
+
+    return render_template(
+        "devhelp/settings.jinja",
+        associated_wallet=associated_wallet,
+        wallets=wallets,
+        cookies=request.cookies,
+    )
+
+
+@devhelp_endpoint.route("/settings", methods=["POST"])
+@login_required
+@user_secret_decrypted_required
+def settings_post():
+    show_menu = request.form["show_menu"]
+    user = app.specter.user_manager.get_user()
+    if show_menu == "yes":
+        user.add_service(DevhelpService.id)
+    else:
+        user.remove_service(DevhelpService.id)
+    used_wallet_alias = request.form.get("used_wallet")
+    if used_wallet_alias != None:
+        wallet = current_user.wallet_manager.get_by_alias(used_wallet_alias)
+        DevhelpService.set_associated_wallet(wallet)
+    return redirect(url_for(f"{DevhelpService.get_blueprint_name()}.settings_get"))
