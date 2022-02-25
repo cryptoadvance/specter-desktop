bf6c6cd0a475ed000118ca40fb7545508510bac6
relativistic electron <relativisticelectron@tutanota.com>
2022-02-25 21:48:56 +0100

    Added body text

--- src/cryptoadvance/specter/templates/includes/notifications.js
+++ src/cryptoadvance/specter/templates/includes/notifications.js
@@ -1,30 +1,47 @@
+function webpush_notification(title, options) {
+    function show_notification(){
+        let notification = new Notification(title, options);
+    };
 
-function webpush_notification(text) {
-    // Let's check if the browser supports notifications
+
+
+    // Let's check if the browser supports notifications    
     if (!("Notification" in window)) {
         console.debug("This browser does not support desktop notification");
     }
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
         // If it's okay let's create a notification
-        let notification = new Notification(text);
+        show_notification();
     }
-
     // Otherwise, we need to ask the user for permission
     else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(function (permission) {
         // If the user accepts, let's create a notification
         if (permission === "granted") {
-            let notification = new Notification(text);
+            show_notification();
         }
         });
     }
 
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them any more.
-    }
+};
 
 
+function webpush_tx(tx) {
+    title = `Incoming Transaction`;
+    amount = tx["amount"];
+    amount_formatted = amount.toFixed(8);
+    if  (amount>0){
+        amount_formatted = `+${amount_formatted}`
+    };
+
+    var options = {
+        body:   amount_formatted,
+    };
+    webpush_notification(title, options);
+};
 
 current_chaintip_height = -1
 function evalaute_new_transactions(){
@@ -35,10 +52,14 @@ function evalaute_new_transactions(){
             // do something with the response
             for (let i in wallets_txlists) {
                 console.log(wallets_txlists[i])
-                webpush_notification(`New Transaction ${wallets_txlists[i]["amount"]}`)
+                webpush_tx(wallets_txlists[i])
             }
 
         });
+};
+
+function notify_new_block(max_chaintip_height){
+    //webpush_notification(`New Block ${max_chaintip_height}`)
 };
 
 function run_scheduled(){ 
@@ -52,9 +73,10 @@ function run_scheduled(){
                 current_chaintip_height = max_chaintip_height;
             }
             if (current_chaintip_height != max_chaintip_height){
+
                 evalaute_new_transactions()
+                notify_new_block(max_chaintip_height)
                 current_chaintip_height = max_chaintip_height;
-                webpush_notification(`New Block ${max_chaintip_height}`)
                 //console.log(`new Block!!!  ${max_chaintip_height}`);
                 // location.reload();
             } ;
@@ -65,4 +87,9 @@ function run_scheduled(){
 
 
 setInterval(run_scheduled, 2000);
+
+
+
+
+
 
