0f530667264176e80aa06eb944a23cf3e8672378
relativisticelectron <60378539+relativisticelectron@users.noreply.github.com>
2022-05-04 17:13:27 +0200

    Feature: Customised front end for adding Electrum devices (#1622)
    * template attribute in the device class added
    * new jinja file for Electrum device (new_device_keys_electrum.jinja)
    * cypress test for electrum device
    
    Co-authored-by: moneymanolis <moneymanolis@protonmail.com>
    Co-authored-by: Kim Neunert <k9ert@gmx.de>

---
+++ cypress/integration/spec_devices.js
@@ -0,0 +1,26 @@
+describe('Test adding different devices', () => {
+    before(() => {
+        cy.viewport(1200,660)
+        cy.visit('/')
+    })
+
+    // Keeps the session cookie alive, Cypress by default clears all cookies before each test
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('session')
+    })
+
+    it('Electrum device', () => {
+        cy.get('#toggle_devices_list').click()
+        cy.get('#btn_new_device').click()
+        cy.get('#electrum_device_card').click()
+        cy.get('#device_name').type("Electron's Electrum Device")
+        // Open and close the explainer
+        cy.get('#toggle-explainer').click()
+        cy.get('#toggle-explainer').click()
+        cy.get('#master_pub_key').type("vpub5VGXXixD2pHLFtcKtCF57e8mx2JW6fie8VydXijC8sRKAL4RshgjEmzbmV915NeVB9pd23DVYem6zWM7HXFLNwaffNVHowdD9SJWwESyQhp")
+        cy.get('.small-card > .btn').click()
+        cy.contains('New device was added successfully!')
+        cy.get('#page_overlay_popup_cancel_button').click()
+        cy.get('#forget_device').click()
+    })
+})
