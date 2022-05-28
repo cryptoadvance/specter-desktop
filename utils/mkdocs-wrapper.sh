2d3129e400d14b8fcc18330eba9410f38aea95e0
relativistic electron <relativisticelectron@tutanota.com>
2022-05-28 22:51:16 +0200

    solved https://github.com/cryptoadvance/specter-desktop/issues/1736

--- utils/mkdocs-wrapper.sh
+++ utils/mkdocs-wrapper.sh
@@ -15,7 +15,7 @@
 # We're using mkdocs for creating the static pages
 # We don't pin this dependency as this is not relevant for either testing or
 # production. Therefore it's easier to simply let it upgrade automatically:
-pip3 install mkdocs mkdocs-video
+pip3 install mkdocs mkdocs-video  mdx_truly_sane_lists
 # At the sime of this comment, we had: mkdocs==1.2.3
 
 
