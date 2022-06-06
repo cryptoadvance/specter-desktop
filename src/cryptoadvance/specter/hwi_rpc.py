2ad42af9beb21d204874d8ac4de0e8c901162bcb
Daniel Newton <djpnewton@gmail.com>
2022-06-06 12:33:28 +1200

    fix issue where the server stalls on startup waiting for a hardware wallet pin to be entered if such a hardware wallet is plugged in when starting the server

--- src/cryptoadvance/specter/hwi_rpc.py
+++ src/cryptoadvance/specter/hwi_rpc.py
@@ -75,11 +75,6 @@ class HWIBridge(JSONRPC):
             "extract_master_blinding_key": self.extract_master_blinding_key,
             "bitbox02_pairing": self.bitbox02_pairing,
         }
-        # Running enumerate after beginning an interaction with a specific device
-        # crashes python or make HWI misbehave. For now we just get all connected
-        # devices once per session and save them.
-        print("Initializing HWI...")  # to explain user why it takes so long
-        self.enumerate()
 
     @locked(hwilock)
     def enumerate(self, passphrase="", chain=""):
