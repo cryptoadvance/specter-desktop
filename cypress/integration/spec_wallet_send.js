f8e9b242493c62ef462d62492002e8774f247404
relativistic electron <relativisticelectron@tutanota.com>
2022-06-15 08:52:17 +0200

    new cypress test not working
    change
    
    Rebase conflict
    
    Commit messages are not applicable!
    (╯°□°）╯︵ ┻━┻

--- cypress/integration/spec_wallet_send.js
+++ cypress/integration/spec_wallet_send.js
@@ -102,6 +102,76 @@ describe('Test sending transactions', () => {
             expect(amount).to.be.lte(5)
         })
         cy.get('#deletepsbt_btn').click()
+    })
+
+
+    it('check drag and drop reordering of recipients and check the fee es deducted corrrectly', () => {
+        // We need new sats but mine2wallet only works if a wallet is selected
+        cy.selectWallet("Test Hot Wallet 1")
+        cy.mine2wallet("btc")
+        cy.get('#btn_send').click()
+        /// The addresses are the first three from DIY ghost
+        cy.get('#address_0').type("bcrt1qvtdx75y4554ngrq6aff3xdqnvjhmct5wck95qs")
+        cy.get('#label_0').type("Recipient 1")
+        cy.get('#amount_0').type(10)
+        cy.get('#toggle_advanced').click()
+        cy.get('main').scrollTo('bottom')
+        cy.get('#add-recipient').click()
+        cy.get('#address_1').type("bcrt1qgzmq6e3tn67kveryf2je6nd3nv4txef4sl8wre")
+        cy.get('#label_1').type("Recipient 2")
+        cy.get('#amount_1').type(5)
+        cy.get('main').scrollTo('bottom')
+        cy.get('#add-recipient').click()
+        cy.get('#address_2').type("bcrt1q9mkrhmxcn7rslzfv6lke8859m7ntwudfjqmcx7")
+        cy.get('#label_2').type("Recipient 3")
+        cy.get('#send_max_2').click()
+        cy.get('main').scrollTo('bottom')
+        // Shadow DOM
+        // Check whether the subtract fees box is ticked
+        cy.get('#fee-selection-component').find('.fee_container').find('input#subtract').invoke('prop', 'checked').should('eq', true)
+        // Check whether the recipient number input field is visible (shadow DOM)
+        cy.get('#fee-selection-component').find('.fee_container').find('span#subtract_from').should('be.visible')
+        // Light DOM
+        // Check the values of the hidden inputs in the light DOM which are used for the form
+        // Note: Despite identical ids the hidden inputs seem to be fetched first since they are higher up in the DOM
+        cy.get('#fee-selection-component').find('#subtract').invoke('attr', 'value').should('eq', 'true')
+        // Send max was applied to the third recipient, so the value (identical with the id) should be 2
+        cy.get('#fee-selection-component').find('.fee_container').find('#subtract_from_recipient_id_select').should('have.value', '2');
+        // the displayed value of id=2 should be 3
+        cy.get('#fee-selection-component').find('.fee_container').find('#subtract_from_recipient_id_select').find(':selected').should('have.text', 'Recipient 3');
+        
+        // Change recipient number to 2    
+        cy.get('#fee-selection-component').find('.fee_container').find('#subtract_from_recipient_id_select').select('Recipient 2')   // html select with cypress: https://www.cypress.io/blog/2020/03/20/working-with-select-elements-and-select2-widgets-in-cypress/
+        cy.get('#fee-selection-component').find('.fee_container').find('#subtract_from_recipient_id_select').should('have.value', '1');
+        // the displayed value of id=1 should be 2
+        cy.get('#fee-selection-component').find('.fee_container').find('#subtract_from_recipient_id_select').find(':selected').should('have.text', 'Recipient 2');
+
+
+        // drag and drop
+        const draggable = cy.get('#recipient_0').get(0)  // Pick up this
+        const droppable = cy.get('#recipient_2').get(0)  // Drop over this
+
+        const draggable_coords = droppable.getBoundingClientRect()
+        draggable.dispatchEvent(new MouseEvent('mousemove', {clientX: draggable_coords.x+5, clientY: draggable_coords.y+5}));
+        draggable.dispatchEvent(new MouseEvent('mousedown'));
+        const droppable_coords = droppable.getBoundingClientRect()
+        draggable.dispatchEvent(new MouseEvent('mousemove', {clientX: 10, clientY: 0}));
+        draggable.dispatchEvent(new MouseEvent('mousemove', {
+            clientX: droppable_coords.x+10,   
+            clientY: droppable_coords.y+10  // A few extra pixels to get the ordering right
+        }));
+        draggable.dispatchEvent(new MouseEvent('mouseup'));        
+
+
+
+
+        // The fee should be subtracted from the third recipient
+        cy.get('#create_psbt_btn').click()
+        cy.get('div.tx_info > :nth-child(3) > :nth-child(1)').then(($div) => {
+            const amount = parseFloat($div.text())
+            expect(amount).to.be.lte(5)
+        })
+        cy.get('#deletepsbt_btn').click()
         // Clean up (Hot Device 1 is still needed below)
         cy.deleteWallet("Test Hot Wallet 1")
     })
