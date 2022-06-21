2052cd67eeb7988a7900b4334a459bb64eab9a10
relativistic electron <relativisticelectron@tutanota.com>
2022-06-21 09:46:42 +0200

    bug fix and error handling

--- src/cryptoadvance/specter/notifications/current_flask_user.py
+++ src/cryptoadvance/specter/notifications/current_flask_user.py
@@ -1,13 +1,28 @@
+import logging
 from flask import current_app as app
 
+logger = logging.getLogger(__name__)
 
-def flash(title, **kwargs):
+
+def flash(message: str, category: str = "message"):
+    if not app or not app.specter or not app.specter.user_manager.get_user():
+        logger.warning(
+            f"app.specter.user_manager.get_user() cannot be accessed. Cannot print the flash notification with title {title} and kwargs {kwargs}"
+        )
+        return
+
     app.specter.notification_manager.flash(
-        title, app.specter.user_manager.get_user().id, **kwargs
+        message, app.specter.user_manager.get_user().id, category
     )
 
 
 def create_and_show(title, **kwargs):
+    if not app or not app.specter or not app.specter.user_manager.get_user():
+        logger.warning(
+            f"app.specter.user_manager.get_user() cannot be accessed. Cannot print the flash notification with title {title} and kwargs {kwargs}"
+        )
+        return
+
     app.specter.notification_manager.create_and_show(
         title, app.specter.user_manager.get_user().id, **kwargs
     )
