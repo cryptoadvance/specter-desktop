852177f0fd9cf9d4d9093bb68684e3f587b09d16
relativistic electron <relativisticelectron@tutanota.com>
2022-07-04 10:50:18 +0200

    working websockets client

--- src/cryptoadvance/specter/websockets_server.py
+++ src/cryptoadvance/specter/websockets_server.py
@@ -8,59 +8,92 @@ import asyncio
 import os
 import time
 import websockets
+import threading
 
 
-domain = "localhost"
-ws_port = "5051"
+class WebsocketsServer:
+    def __init__(self):
+        self.domain = "localhost"
+        self.port = "5051"
+        self.connections = set()
+
+    async def register(self, websocket):
+        print(f"register {websocket}")
+        self.connections.add(websocket)
+
+    async def unregister(self, websocket):
+        print(f"unregister {websocket}")
+        self.connections.remove(websocket)
+
+    async def send_messages_to_all(self, message, websocket):
+        if self.connections:
+            print(f"Sending messages to all {self.connections}")
+            await asyncio.wait(
+                [
+                    connection.send(f"Server answers {message}")
+                    for connection in list(self.connections)
+                ]
+            )
+        else:
+            logger.warning(f'connection_list is empty. Nowhere to send "{message}".')
+
+    async def handler(self, websocket, path):  # don't remove path
+        await self.register(websocket)
+        try:
+            await websocket.send("Connected")
+            async for message in websocket:  # this is an endless loop waiting for incoming websocket messages
+                print(f"Do sync stuff with message: {message}")
+                await self.send_messages_to_all(message, websocket)
+        finally:
+            await self.unregister(websocket)
+
+    def start_forever_websockets_server(self):
+        print("Starting websocker server")
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        ws_server = websockets.serve(self.handler, self.domain, self.port)
+
+        loop.run_until_complete(ws_server)
+        loop.run_forever()  # this is missing
+        loop.close()
+
+    def start_server_thread(self, in_new_thread=True):
+        if in_new_thread:
+            t = threading.Thread(target=self.start_forever_websockets_server)
+            t.start()
+        else:
+            self.start_forever_websockets_server()
 
 
-CONNECTIONS = set()
+class WebsocketsClient:
+    def __init__(self):
+        self.domain = "localhost"
+        self.port = "5051"
+
+    async def send_message(self, message, expected_answers=2):
+        async with websockets.connect(f"ws://{self.domain}:{self.port}") as websocket:
+            print("Client: connected")
+            await websocket.send(message)
+            for i in range(expected_answers):
+                msg = await websocket.recv()
+                print(f"Client: Answer {i} from server: {msg}")
 
 
-async def register(websocket):
-    print(f"register {websocket}")
-    CONNECTIONS.add(websocket)
+ws = WebsocketsServer()
+ws.start_server_thread()
 
 
-async def unregister(websocket):
-    CONNECTIONS.remove(websocket)
+client = WebsocketsClient()
 
 
-async def notify_users(message, websocket):
-    connection_list = []
-    for connection in CONNECTIONS:
-        connection_list.append(connection)
-
-    if connection_list:
-        await asyncio.wait(
-            [
-                connection.send(f"Server answers {message}")
-                for connection in connection_list
-            ]
-        )
-    else:
-        print(f'connection_list is empty. Nowhere to send "{message}".')
+async def f():
+    await client.send_message(f"Main thread: loop {i}")
 
 
-async def message_control(websocket, path):
-    await register(websocket)
-    try:
-        await websocket.send("Connected")
-        async for message in websocket:
-            print(message)
-            await notify_users(message, websocket)
-    finally:
-        await unregister(websocket)
-
-
-print("\nStarting websocker server")
-start_server = websockets.serve(message_control, domain, ws_port)
-
-# this should make it also wiork in multithreading  https://stackoverflow.com/questions/58617631/python3-websocket-in-thread
-try:
-    event_loop = asyncio.get_event_loop()
-except:
-    event_loop = asyncio.new_event_loop()
-
-event_loop.run_until_complete(start_server)
-event_loop.run_forever()
+for i in range(1000):
+    print(
+        f"Loop {i} --------------------------------------------------------------------------"
+    )
+    time.sleep(2)
+    asyncio.get_event_loop().run_until_complete(f())
