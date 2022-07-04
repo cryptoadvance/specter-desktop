a5b21591e9cecce54ec306d00c02ad35b56769f9
relativistic electron <relativisticelectron@tutanota.com>
2022-07-04 11:35:31 +0200

    working sending 1 answer back

--- src/cryptoadvance/specter/websockets_server.py
+++ src/cryptoadvance/specter/websockets_server.py
@@ -24,31 +24,34 @@ class WebsocketsServer:
         self.connected_websockets.remove(websocket)
 
     async def send_messages_to_all_connected_websockets(
-        self, message, connected_websockets=None
+        self, message, connected_websockets=None, exclude_websockets=None
     ):
         if connected_websockets is None:
             connected_websockets = self.connected_websockets
+        connected_websockets = set(connected_websockets)
+        for ws in exclude_websockets:
+            connected_websockets.remove(ws)
 
         if connected_websockets:
-            print(f"Sending messages to all {connected_websockets}")
             await asyncio.wait(
                 [
                     connection.send(f"Server answers {message}")
                     for connection in list(connected_websockets)
                 ]
             )
+            return "Successfully sent {message} to {connected_websockets}"
         else:
-            logger.warning(
-                f'connected_websockets is empty. Nowhere to send "{message}".'
-            )
+            return f'connected_websockets is empty. Nowhere to send "{message}"'
 
     async def handler(self, websocket, path):  # don't remove path
         await self.register(websocket)
         try:
-            await websocket.send("Connected")
             async for message in websocket:  # this is an endless loop waiting for incoming websocket messages
                 print(f"Do sync stuff with message: {message}")
-                await self.send_messages_to_all_connected_websockets(message)
+                msg = await self.send_messages_to_all_connected_websockets(
+                    message, exclude_websockets={websocket}
+                )
+                await websocket.send(msg)
         finally:
             await self.unregister(websocket)
 
@@ -76,7 +79,7 @@ class WebsocketsClient:
         self.domain = "localhost"
         self.port = "5051"
 
-    async def send_message(self, message, expected_answers=2):
+    async def send_message(self, message, expected_answers=1):
         messages = []
         async with websockets.connect(f"ws://{self.domain}:{self.port}") as websocket:
             print("Client: connected")
