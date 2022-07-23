be6a3b45628bf224574746e59059ef212daf40dd
relativistic electron <relativisticelectron@tutanota.com>
2022-07-23 10:40:50 +0200

    minimal working example for self-signed ssl

---
+++ src/cryptoadvance/specter/temp_echoserver.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import simple_websocket
+
+app = Flask(__name__)
+
+
+@app.route("/echo", websocket=True)
+def echo():
+    ws = simple_websocket.Server(request.environ)
+    try:
+        while True:
+            data = ws.receive()
+            print(data)
+            ws.send(data)
+    except simple_websocket.ConnectionClosed:
+        pass
+    return ""
+
+
+@app.route("/")
+def main():
+    return ""
+
+
+from pathlib import Path
+
+if __name__ == "__main__":
+
+    app.run(
+        ssl_context=(
+            f"{Path.home()}/.specter_dev/cert.pem",
+            f"{Path.home()}/.specter_dev/key.pem",
+        )
+    )
+
+
+# Do this in the console after running the server
+# var websocket = new WebSocket(`wss://127.0.0.1:5000/echo`)
+# websocket.onmessage = function(message) {
+#             console.log(message)
+#         };
+
+# websocket.send('dd2d')
