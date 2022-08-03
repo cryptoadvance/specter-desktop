b690ce937028bb8011ab4a5a7798354b37f11e8c
relativistic electron <relativisticelectron@tutanota.com>
2022-08-03 08:02:19 +0200

    better formatting

--- src/cryptoadvance/specter/global_search.py
+++ src/cryptoadvance/specter/global_search.py
@@ -255,7 +255,7 @@ class GlobalSearchTrees:
             search_function=lambda x: self._search_in_structure(
                 x,
                 [psbt.to_dict() for psbt in wallet.pending_psbts.values()],
-                title_key="txid",
+                title_key="address",
             ),
         )
 
