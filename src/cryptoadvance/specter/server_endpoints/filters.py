6669431f03dc53bd03a854ad6aae55153c352d32
relativistic electron <relativisticelectron@tutanota.com>
2022-08-16 14:43:33 +0200

    better amount display

--- src/cryptoadvance/specter/server_endpoints/filters.py
+++ src/cryptoadvance/specter/server_endpoints/filters.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from operator import invert
 from flask import current_app as app
 from flask import Blueprint
 from jinja2 import pass_context
@@ -28,6 +29,24 @@ def timedatetime(context, s):
 
 
 @pass_context
+@filters_bp.app_template_filter("any_attribute")
+def any_attribute(context, values, attribute=None):
+    return any([getattr(value, attribute) for value in values])
+
+
+@pass_context
+@filters_bp.app_template_filter("sum_with_subdicts")
+def sum_with_subdicts(context, dicts, attribute=None):
+    summed_dict = {}
+    for d in dicts:
+        for k, v in getattr(d, attribute).items():
+            if k not in summed_dict:
+                summed_dict[k] = 0
+            summed_dict[k] += v
+    return summed_dict
+
+
+@pass_context
 @filters_bp.app_template_filter("btcamount")
 def btcamount(context, value):
     if value is None:
@@ -35,7 +54,24 @@ def btcamount(context, value):
     if value < 0 and app.specter.is_liquid:
         return "Confidential"
     value = round(float(value), 8)
-    return "{:,.8f}".format(value).rstrip("0").rstrip(".")
+
+    def replace_char(text, position, new_str):
+        return "".join([(c if i != position else new_str) for i, c in enumerate(text)])
+
+    formatted_amount = "{:,.8f}".format(value)
+    # strip last digits for better readability and replace with invisible characters
+    for i in reversed(range(len(formatted_amount))):
+        if formatted_amount[i] == "0":
+            formatted_amount = replace_char(
+                formatted_amount, i, " "
+            )  # the https://unicode-table.com/en/2007/
+            continue
+        elif formatted_amount[i] == ".":
+            formatted_amount = replace_char(
+                formatted_amount, i, " "
+            )  # https://unicode-table.com/en/2008/
+        break
+    return formatted_amount
 
 
 @pass_context
@@ -54,6 +90,21 @@ def feerate(context, value):
     if value <= 1.02:
         value = 1
     return "{:,.2f}".format(value).rstrip("0").rstrip(".")
+
+
+@pass_context
+@filters_bp.app_template_filter("btcunitamount")
+def btcunitamount(context, value):
+    if app.specter.hide_sensitive_info:
+        return "#########"
+    if value is None:
+        return "Unknown"
+    if value < 0 and app.specter.is_liquid:
+        return "Confidential"
+    if app.specter.unit != "sat":
+        return btcamount(context, value)
+    value = float(value)
+    return "{:,.0f}".format(round(value * 1e8))
 
 
 @pass_context
