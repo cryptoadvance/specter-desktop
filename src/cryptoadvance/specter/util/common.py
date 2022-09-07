433a91112bf28e99703c95d8efd88c1a622cf7ef
relativistic electron <relativisticelectron@tutanota.com>
2022-09-07 07:29:56 +0200

    format_btc_amount_as_sats with enable_digit_formatting

--- src/cryptoadvance/specter/util/common.py
+++ src/cryptoadvance/specter/util/common.py
@@ -30,8 +30,26 @@ def snake_case2camelcase(word):
     return "".join(x.capitalize() or "_" for x in word.split("_"))
 
 
-def format_btc_amount_as_sats(value: Union[float, str]) -> str:
-    return "{:,.0f}".format(round(float(value) * 1e8))
+def format_btc_amount_as_sats(
+    value: Union[float, str], enable_digit_formatting=True
+) -> str:
+    array = list("{:,.0f}".format(round(float(value) * 1e8)))
+
+    if enable_digit_formatting:
+        if len(array) >= 4:
+            left_index = -6 if len(array) >= 6 else -len(array)
+            array[
+                left_index
+            ] = f'<span class="thousand-digits-in-btc-amount">{array[left_index]}'
+            array[-4] = f"{array[-4]}</span>"
+
+        left_index = -3 if len(array) >= 3 else -len(array)
+        array[
+            left_index
+        ] = f'<span class="last-digits-in-btc-amount">{array[left_index]}'
+        array[-1] = f"{array[-1]}</span>"
+
+    return "".join(array)
 
 
 def format_btc_amount(
