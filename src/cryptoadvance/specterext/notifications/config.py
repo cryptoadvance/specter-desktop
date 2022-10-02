3f2dd389ca4b1fe1d0074b6b2bcc2fbd9153f16d
relativistic electron <relativisticelectron@tutanota.com>
2022-10-02 15:43:26 +0200

    reenable websockets for cypress

--- src/cryptoadvance/specterext/notifications/config.py
+++ src/cryptoadvance/specterext/notifications/config.py
@@ -19,3 +19,7 @@ class TestConfig(BaseConfig):
     """This is a extension-based Config for Production"""
 
     SPECTER_NOTIFICATIONS_WEBSOCKETS_ENABLED = False
+
+
+class CypressTestConfig(TestConfig):
+    SPECTER_NOTIFICATIONS_WEBSOCKETS_ENABLED = True
