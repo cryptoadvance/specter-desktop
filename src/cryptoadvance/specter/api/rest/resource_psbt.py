73b9ee2a80f152046e553677079ea5ab6eb1ec25
moneymanolis <moneymanolis@protonmail.com>
2022-10-19 21:27:56 +0200

    fix for rest test

--- src/cryptoadvance/specter/api/rest/resource_psbt.py
+++ src/cryptoadvance/specter/api/rest/resource_psbt.py
@@ -1,13 +1,16 @@
 import logging
+import base64
+from json import dumps
 
 from .base import (
     SecureResource,
     rest_resource,
 )
-from flask import current_app as app, request
+
+from flask import current_app as app, request, Response
 from ...wallet import Wallet
 from ...commands.psbt_creator import PsbtCreator
-
+from ...specter_error import SpecterError
 from .. import auth
 
 logger = logging.getLogger(__name__)
@@ -20,11 +23,16 @@ class ResourcePsbt(SecureResource):
     endpoints = ["/v1alpha/wallets/<wallet_alias>/psbt"]
 
     def get(self, wallet_alias):
-        # ToDo: check whether the user has access to the wallet
         user = auth.current_user()
-        wallet: Wallet = app.specter.user_manager.get_user(
-            user
-        ).wallet_manager.get_by_alias(wallet_alias)
+        wallet_manager = app.specter.user_manager.get_user(user).wallet_manager
+        # Check that the wallet belongs to the user from Basic Auth
+        try:
+            wallet = wallet_manager.get_by_alias(wallet_alias)
+        except SpecterError:
+            error_message = dumps(
+                {"message": "The wallet does not belong to the user in the request."}
+            )
+            return Response(error_message, 403)
         pending_psbts = wallet.pending_psbts_dict()
         return {"result": pending_psbts or {}}
 
