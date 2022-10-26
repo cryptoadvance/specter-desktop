fd047fe375eef862b12289d16395af79d988f4e3
moneymanolis <moneymanolis@protonmail.com>
2022-10-26 11:54:23 +0200

    tidying up of spec_format.js + tests for variables in the Specter object & first test of a format function (unitLabel)

--- cypress/integration/spec_format.js
+++ cypress/integration/spec_format.js
@@ -1,45 +1,68 @@
-
-/* setting global js variables for helpers.js */
-// const is_liquid = false;
-const is_testnet = true;
-const price_check_enabled = false;
-const hide_sensitive_info_enabled = false;
-const specter_unit = 'BTC';
-const alt_rate = '10001';
-const alt_symbol = '$';
-
-// import { capitalize, formatUnitLabel , formatLiquidUnitLabel, rstrip, formatLiquidAmountAndUnitArray,
-// 	formatLiquidAmountsAndUnitsArray, formatLiquidAmountsAndUnits,
-// 	formatBtcAmountAndUnitArray, formatBtcAmountAndUnit, formatBtcAmount, formatPrice } from '../../src/cryptoadvance/specter/static/helper-modules/formatting.js'
-    
-import { Specter } from '../../src/cryptoadvance/specter/static/helpers.js'
-
 describe('Tests of JS format functions', () => {
     before(() => {
         cy.visit('/')
     })
+    
     it('Test capitalize function', () => {
-        expect(window.Specter.format.capitalize('satoshi')).to.equal('Satoshi') // window is needed here to access Specter object
-        expect(window.Specter.format.capitalize('satoshi nakamoto')).to.equal('Satoshi nakamoto')
+        cy.window()
+            .then((win) => {
+                expect(win.Specter.format.capitalize('satoshi')).to.equal('Satoshi') // window is needed here to access Specter object
+                expect(win.Specter.format.capitalize('satoshi nakamoto')).to.equal('Satoshi nakamoto')
+            })
     })
 
     it('Test rstrip', () => {
-        expect(window.Specter.format.rstrip('10000000', '0')).to.equal('1')
-        expect(window.Specter.format.rstrip('liaaaaaaa', 'a')).to.equal('li')
-        expect(window.Specter.format.rstrip('10,', ',')).to.equal('10')
+        cy.window()
+            .then((win) => {
+                expect(win.Specter.format.rstrip('10000000', '0')).to.equal('1')
+                expect(win.Specter.format.rstrip('liaaaaaaa', 'a')).to.equal('li')
+                expect(win.Specter.format.rstrip('10,', ',')).to.equal('10')
+            })
     })
 
-    it.skip('Test unitLabel', () => {
-        expect(window.Specter.format.unitLabel('btc', false, false, true)).to.equal('BTC')
+    it('Check the global variables in the Specter window object', () => {
+        cy.window().its('Specter.unit').should('equal', 'btc')
+        cy.window().its('Specter.isTestnet').should('equal', true)
+        cy.window().its('Specter.isLiquid').should('equal', false)
+        cy.window().its('Specter.priceCheck').should('equal', false)
+        cy.window().its('Specter.hideSensitiveInfo').should('equal', false)
+        cy.window().its('Specter.altRate').should('equal', "1")
+        cy.window().its('Specter.altSymbol').should('equal', "BTC")
+        // Alternative approach
+        let targetUnit
+        cy.window()
+            .then((win) => {
+                targetUnit = win.Specter.unit;
+            })
+            .then(() => {
+                expect(targetUnit).to.equal('btc')
+            })
+    }) 
+
+    it('Test unitLabel', () => {
+        // Not completely sure why just expect(window.Specter.format.unitLabel(false)).to.equal('tBTC') is not working
+        // It probably has to do with this: https://docs.cypress.io/api/commands/window#Cypress-uses-2-different-windows
+        cy.window()
+            .then((win) => {
+                expect(win.Specter.format.unitLabel(false)).to.equal('tBTC')
+                win.Specter.isLiquid = true // This is setting the property across tests, so we need to reset it.
+                expect(win.Specter.format.unitLabel(false)).to.equal('tLBTC')
+                win.Specter.unit = "sat"
+                expect(win.Specter.format.unitLabel(false)).to.equal('tLsat')
+                win.Specter.isLiquid = false
+                expect(win.Specter.format.unitLabel(false)).to.equal('tsat')
+                win.Specter.isTestnet = false
+                expect(win.Specter.format.unitLabel(false)).to.equal('sat')
+                win.Specter.isLiquid = true
+                expect(win.Specter.format.unitLabel(false)).to.equal('Lsat')
+                win.Specter.unit = "btc"
+                expect(win.Specter.format.unitLabel(false)).to.equal('LBTC')
+                win.Specter.isLiquid = false
+                expect(win.Specter.format.unitLabel(false)).to.equal('BTC')
+                // Reset to testnet again
+                win.Specter.isTestnet = true
+            })
     })
 
-    it.skip('Test price', () => {
-        expect(window.Specter.price(0.1, '$', 10001, true)).to.equal('tobedone')
-    })
 
-    it.skip('Test price', () => {
-        expect(window.Specter.price(0.1, '$', 10001, true)).to.equal('tobedone')
-    })
-
-        
 })
