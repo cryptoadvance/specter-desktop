65bb7d2309845270e123718b9ab17bf1d94077d5
k9ert <k9ert@gmx.de>
2022-11-03 10:01:43 +0100

    Chore: Refactor Swan Extension to cryptoadvance.specterext (#1955)
    * Refactor Swan service, delete bitcoinreserve
    
    fix tests for services move to specterext and complete
    
    * complete refactoring of swan extension
    
    * added a test
    
    * feedback manolis
    
    Co-authored-by: Manolis Mandrapilias <70536101+moneymanolis@users.noreply.github.com>

--- tests/test_managers_service.py
+++ tests/test_managers_service.py
@@ -16,7 +16,6 @@ def test_ServiceManager2(mock_specter, mock_flaskapp, caplog):
     # So the ServiceManager will move up the dependency tree of TestConfig until it finds
     # a Config and will copy the keys into the flask-config
     assert mock_flaskapp.config["SWAN_API_URL"] == "https://api.dev.swanbitcoin.com"
-    assert sm.services["bitcoinreserve"] != None
     assert sm.services["swan"] != None
 
     sm.execute_ext_callbacks(after_serverpy_init_app, scheduler=None)
@@ -59,8 +58,7 @@ def test_ServiceManager_get_service_x_dirs(caplog):
 
 def test_ServiceManager_get_service_packages():
     packages = ServiceManager.get_service_packages()
-    assert "cryptoadvance.specter.services.swan.service" in packages
-    assert "cryptoadvance.specter.services.bitcoinreserve.service" in packages
+    assert "cryptoadvance.specterext.swan.service" in packages
 
 
 @pytest.fixture
@@ -75,8 +73,7 @@ def mock_flaskapp(mock_specter):
 
     flaskapp_mock = Flask(__name__)
     flaskapp_mock.config["EXTENSION_LIST"] = [
-        "cryptoadvance.specter.services.swan.service",
-        "cryptoadvance.specter.services.bitcoinreserve.service",
+        "cryptoadvance.specterext.swan.service",
     ]
     flaskapp_mock.config["ISOLATED_CLIENT_EXT_URL_PREFIX"] = "/spc/ext"
     flaskapp_mock.config["EXT_URL_PREFIX"] = "/ext"
