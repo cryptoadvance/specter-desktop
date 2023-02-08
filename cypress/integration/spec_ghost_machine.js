4328c654a7ccba2d6d51f0f40dcdf0f7c3443fbe
Shivoham Angal <66953222+Shivoham102@users.noreply.github.com>
2023-02-08 14:02:29 +0530

    Bugfix: Fixed Save Backup PDF Button after creating wallet (#2149)
    * added fonts and changed childnode index
    
    * Adding pdf-tests
    
    * new parameter for wallet creation and fix nasty children logic
    
    ---------
    
    Co-authored-by: Manolis Mandrapilias <70536101+moneymanolis@users.noreply.github.com>
    Co-authored-by: Kim Neunert <k9ert@gmx.de>

--- cypress/integration/spec_ghost_machine.js
+++ cypress/integration/spec_ghost_machine.js
@@ -6,11 +6,38 @@ describe('Ghost machine', () => {
             .then($body => {
                 if ($body.find('[data-cy="no-core-connection"]').length) {
                     cy.connect()
-                    cy.addWallet('Ghost wallet', 'segwit', 'funded', 'btc', 'singlesig', 'DIY ghost')
+                    cy.addWallet('Ghost wallet', 'segwit', false, false, 'btc', 'singlesig', 'DIY ghost')
                 }
                 else {
-                    cy.addWallet('Ghost wallet', 'segwit', 'funded', 'btc', 'singlesig', 'DIY ghost')
+                    cy.addWallet('Ghost wallet', 'segwit', false, false, 'btc', 'singlesig', 'DIY ghost')
                 }
+                cy.get('#pdf-wallet-download').click()
+                cy.readFile(`./cypress/downloads/ghost_wallet_backup.pdf`)
+                cy.get('[data-cy="new-wallet-added-overlay-close-btn"]').click()
+                cy.mine2wallet('btc')
             })
+    })
+
+    it('has downloadable artifacts', () => {
+        cy.selectWallet('Ghost wallet')
+        cy.get('#btn_settings').click()
+        cy.contains("DIY ghost")
+
+        cy.get('#btn_settings').click()
+        cy.get('#advanced_settings_tab_btn').click()
+        cy.contains("Import address labels")
+
+        cy.get('#export_settings_tab_btn').click()
+        cy.get('#export_specter_format').click()
+        cy.readFile(`./cypress/downloads/Ghost wallet.json`)
+
+        cy.get('#pdf-wallet-download').click()
+        cy.readFile(`./cypress/downloads/ghost_wallet_backup.pdf`)
+
+        cy.get('#pdf-paperxpub-download').click()
+        cy.readFile(`./cypress/downloads/ghost_wallet_uj_backup.pdf`)
+
+        cy.get('#electrum_export').click()
+        cy.readFile(`./cypress/downloads/ghost_wallet_electrum.backup`)
     })
 })
