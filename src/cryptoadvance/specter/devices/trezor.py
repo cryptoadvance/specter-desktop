63a62b5b3b747b6501a52a419aafb6ebae8adeab
moneymanolis <moneymanolis@protonmail.com>
2023-03-08 19:09:16 +0100

    set taproot support to true for trezor

--- src/cryptoadvance/specter/devices/trezor.py
+++ src/cryptoadvance/specter/devices/trezor.py
@@ -6,9 +6,9 @@ class Trezor(HWIDevice):
     device_type = "trezor"
     name = "Trezor"
     icon = "img/devices/trezor_icon.svg"
-
     supports_hwi_toggle_passphrase = True
     supports_hwi_multisig_display_address = True
+    taproot_support = True
 
     @classmethod
     def get_client(cls, *args, **kwargs):
