9f199708cf46df537bcbc60a3b4f08cd4b5e8c94
Kim Neunert <k9ert@gmx.de>
2023-03-20 12:25:26 +0100

    documentation

--- src/cryptoadvance/specterext/hwi/config.py
+++ src/cryptoadvance/specterext/hwi/config.py
@@ -14,12 +14,20 @@ class BaseConfig:
     HWI_BIN_VERSION = "2.2.1"
     HWI_BIN_HASH = "93ed5fc6eb7d66a466e84d3fd0601fd30b312c8bed4576cd3e78e9e281976816"
 
+    HWI_RPC_IMPL = os.getenv("HWI_RPC_IMPL", "bin")
+
 
 class DevelopmentConfig(BaseConfig):
-    HWI_RPC_IMPL = os.getenv("HWI_RPC_IMPL", "bin")
+    pass
 
 
 class ProductionConfig(BaseConfig):
     """This is a extension-based Config for Production"""
 
-    HWI_RPC_IMPL = "lib"
+    pass
+
+    # In the pyinstaller/main.js is this variable defined as "lib"
+    # However in pip-installations, we assume "bin"
+    # in order to break free of dependency restrictions
+    # So no need for this:
+    # HWI_RPC_IMPL = os.getenv("HWI_RPC_IMPL", "lib")
