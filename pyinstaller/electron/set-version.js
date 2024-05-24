2baafe0008a6bec4d36747bd7483151cf9674e6a
Kim Neunert <kim@swanbitcoin.com>
2024-05-24 15:45:42 +0200

    adjust set-version logic

--- pyinstaller/electron/set-version.js
+++ pyinstaller/electron/set-version.js
@@ -1,20 +1,46 @@
+/*
+    This will set the version for package.json and might also create a version-data.json file
+
+
+
+*/
+
 const fs = require('fs')
 const crypto = require('crypto')
-const version = process.argv[2]
+const versionDataFile = './version-data.json'
 
 async function setVersion() {
+    const version = process.argv[2]
+    const file = process.argv[4]
+
+    // Set version in package.json
     let package = require('./package.json')
     package.version = version
     fs.writeFileSync('./package.json', JSON.stringify(package, undefined, 2))
     
-    if (process.argv[3]) {
-        let versionData = {
-            version,
-            sha256: (await createHashFromFile(process.argv[3]))
+    // Set version in version-data.json
+    if (arch && file) {
+        let versionData
+        try {
+           versionData = require(versionDataFile)
+        } catch (error) {
+            console.log(`No ${versionDataFile} found. Creating anew`)
+            versionData = {
+                version,
+                sha256: {}
+            }
         }
+        if (versionData.version != version) {
+            throw new Error(`param version ${version} and version from versionData ${versionData.version} does not match`)
+        }
+        versionData.sha256[process.arch] = (await createHashFromFile(file))
     
-        fs.writeFileSync('./version-data.json', JSON.stringify(versionData, undefined, 2))
-    }    
+        fs.writeFileSync(versionDataFile, JSON.stringify(versionData, undefined, 2))
+        console.log(versionData)
+    } else if (arch || file) {
+        throw new Error("Declare arch and file or none")
+    }
+
 }
 
 const createHashFromFile = filePath => new Promise(resolve => {
