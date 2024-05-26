00a3b9ca872cc4d0051e4eabcd1a5f8f6046a9d7
Kim Neunert <kim@swanbitcoin.com>
2024-05-26 10:24:40 +0200

    set-version now deleting the file instead of complaining if version-mismatch

--- pyinstaller/electron/set-version.js
+++ pyinstaller/electron/set-version.js
@@ -1,52 +1,57 @@
-/*
-    This will set the version for package.json and might also create a version-data.json file
-
-
-
-*/
-
-const fs = require('fs')
-const crypto = require('crypto')
-const versionDataFile = './version-data.json'
+const fs = require('fs');
+const crypto = require('crypto');
+const versionDataFile = './version-data.json';
 
 async function setVersion() {
-    const version = process.argv[2]
-    const file = process.argv[3]
-    const arch = process.argv[4] || process.arch
+  const version = process.argv[2];
+  const file = process.argv[3];
+  const arch = process.argv[4] || process.arch;
 
-    // Set version in package.json
-    let package = require('./package.json')
-    package.version = version
-    fs.writeFileSync('./package.json', JSON.stringify(package, undefined, 2))
-    
-    // Set version in version-data.json
-    if (version && file) {
-        let versionData
-        try {
-           versionData = require(versionDataFile)
-        } catch (error) {
-            console.log(`No ${versionDataFile} found. Creating anew`)
-            versionData = {
-                version,
-                sha256: {}
-            }
-        }
-        if (versionData.version != version) {
-            throw new Error(`param version ${version} and version from versionData ${versionData.version} does not match`)
-        }
-        versionData.sha256[arch] = (await createHashFromFile(file))
-    
-        fs.writeFileSync(versionDataFile, JSON.stringify(versionData, undefined, 2))
-        console.log(versionData)
-    } else if (arch || file) {
-        throw new Error("Declare arch and file or none")
+  // Set version in package.json
+  let packageJson = require('./package.json');
+  packageJson.version = version;
+  fs.writeFileSync('./package.json', JSON.stringify(packageJson, undefined, 2));
+
+  // Set version in version-data.json
+  if (version && file) {
+    let versionData;
+    try {
+      versionData = require(versionDataFile);
+      
+      if (versionData.version != version) {
+        console.log(`Version mismatch. Deleting ${versionDataFile} and creating anew.`);
+        fs.unlinkSync(versionDataFile); // Delete the existing version-data.json file
+        versionData = createNewVersionData(version); // Create new version data object
+      }
+      
+    } catch (error) {
+      console.log(`No ${versionDataFile} found. Creating anew.`);
+      versionData = createNewVersionData(version);
     }
-
+    // Compute SHA256 hash of the provided file
+    versionData.sha256[arch] = await createHashFromFile(file);
+    // Write new version data to file
+    fs.writeFileSync(versionDataFile, JSON.stringify(versionData, undefined, 2));
+    console.log(versionData);
+  } else if (arch || file) {
+    throw new Error("Declare both arch and file or none.");
+  }
 }
 
-const createHashFromFile = filePath => new Promise(resolve => {
-    const hash = crypto.createHash('sha256');
-    fs.createReadStream(filePath).on('data', data => hash.update(data)).on('end', () => resolve(hash.digest('hex')));
+function createNewVersionData(version) {
+  // Return a new version data object
+  return {
+    version,
+    sha256: {}
+  };
+}
+
+const createHashFromFile = filePath => new Promise((resolve, reject) => {
+  const hash = crypto.createHash('sha256');
+  fs.createReadStream(filePath)
+    .on('data', data => hash.update(data))
+    .on('end', () => resolve(hash.digest('hex')))
+    .on('error', reject);
 });
 
-setVersion()
+setVersion().catch(console.error);
