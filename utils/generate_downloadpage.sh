3de967389a5927336d9ba6ebdcc5c710e8c39a1d
Kim Neunert <kim@swanbitcoin.com>
2024-05-28 18:37:10 +0200

    kick again and again

--- utils/generate_downloadpage.sh
+++ utils/generate_downloadpage.sh
@@ -18,7 +18,7 @@ install_gh_macos() {
 install_gh_linux() {
   echo "Installing GitHub CLI using official script..."
   curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+  chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
   apt update
   apt install gh
