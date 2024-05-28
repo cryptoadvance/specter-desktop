ce0715423992920e0f867a3e11764a84bd154caa
Kim Neunert <kim@swanbitcoin.com>
2024-05-28 18:22:31 +0200

    kick

--- utils/generate_downloadpage.sh
+++ utils/generate_downloadpage.sh
@@ -17,11 +17,11 @@ install_gh_macos() {
 # Function to install GitHub CLI on Linux
 install_gh_linux() {
   echo "Installing GitHub CLI using official script..."
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
   sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-  sudo apt update
-  sudo apt install gh
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  apt update
+  apt install gh
 }
 
 function prereq {
