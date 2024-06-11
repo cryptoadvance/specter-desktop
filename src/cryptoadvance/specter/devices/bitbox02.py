8a36995c834d713e7d529ff86feaf651b3ca0739
k9ert <k9ert@gmx.de>
2024-06-11 11:58:20 +0200

    Feature: HWI upgrade to 2.4.x (#2452) (Trezor 3 support)
    * hwi upgrade
    
    * making bitbox02 work with hwi 2.2.1
    
    * upgraded yet another bunch of dependencies
    
    * upgrade hwi to 2.4.0
    
    * removing unnecessary dependency

--- src/cryptoadvance/specter/devices/bitbox02.py
+++ src/cryptoadvance/specter/devices/bitbox02.py
@@ -6,3 +6,15 @@ class BitBox02(HWIDevice):
     name = "BitBox02"
     icon = "img/devices/bitbox02_icon.svg"
     supports_hwi_multisig_display_address = True
+
+    @classmethod
+    def get_client(cls, *args, **kwargs):
+        # Convert args tuple to a list to modify it
+        args_list = list(args)
+
+        if args_list[1] == "":  # passphrase
+            # prevent this error-message raised from hwilib since about 2.2.1:
+            # "Internal error: The BitBox02 does not accept a passphrase from the host. Please enable the passphrase option and enter the passphrase on the device during unlock."
+            # IMHO hwilib should treat "" same as none but it does not
+            args_list[1] = None
+        return super().get_client(*args_list, **kwargs)
