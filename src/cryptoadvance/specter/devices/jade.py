aeea104cece02c679e6e3a9b5608efbdb793191c
Manolis <moneymanolis@protonmail.com>
2025-01-01 21:06:43 +0100

    improve readability of code, some comments and example

--- src/cryptoadvance/specter/devices/jade.py
+++ src/cryptoadvance/specter/devices/jade.py
@@ -48,15 +48,13 @@ class Jade(HWIDevice):
         psbts["qrcode"] = f"{self.supported_qr_code_format}:{qr_psbt}"
         return psbts
 
+    # Enables the export of a multisig wallet to a Jade via QR
     def export_wallet(self, wallet):
         if not wallet.is_multisig:
             return None
-        # Jade uses ColdCard's style
+        # Jade uses ColdCard's style (assumes derivaion paths of the keys to be the same)
         CC_TYPES = {"legacy": "BIP45", "p2sh-segwit": "P2WSH-P2SH", "bech32": "P2WSH"}
-        # try to find at least one derivation
-        # cc assume the same derivation for all keys :(
         derivation = None
-        # find correct key
         for k in wallet.keys:
             if k in self.keys and k.derivation != "":
                 derivation = k.derivation.replace("h", "'")
@@ -64,12 +62,12 @@ class Jade(HWIDevice):
         if derivation is None:
             return None
         qr_string = """
-Name: {}
-Policy: {} of {}
-Derivation: {}
-Format: {}
-Sorted: {}
-""".format(
+        Name: {}
+        Policy: {} of {}
+        Derivation: {}
+        Format: {}
+        Sorted: {}
+        """.format(
             to_ascii20(wallet.name),
             wallet.sigs_required,
             len(wallet.keys),
@@ -77,11 +75,26 @@ Sorted: {}
             CC_TYPES[wallet.address_type],
             not wallet.uses_multi,
         )
+
         for k in wallet.keys:
-            # cc assumes fingerprint is known
             fingerprint = k.fingerprint
             if fingerprint == "":
                 fingerprint = get_xpub_fingerprint(k.xpub).hex()
-            qr_string += "{}: {}\n".format(fingerprint.upper(), k.xpub)
+            qr_string += "{}: {}\n".format(
+                fingerprint.upper(),
+                k.xpub,
+            )
+
         qr_string = b2a_base64(qr_string.encode()).decode()
         return f"ur-bytes:{qr_string}"
+
+        # Example output:
+        # """
+        # Name: MyWallet
+        # Policy: 2 of 3
+        # Derivation: m/48'/1'/0'/2'
+        # Format: P2WSH
+        # Sorted: False
+        # A1B2C3D4: tpubD6NzVbkrYhZ...
+        # F2E3D4C5: tpubE6NzVhkxF7Q...
+        # """
